31918b11317031b7212ca1872bfdc51a284fe65d
TomCanTech <tomcantech@gmail.com>
2024-09-28 22:09:38 +0100

    Added list widget and tested entries page

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,6 @@
-use ratatui::{text::Text,layout::{self, Constraint, Layout, Rect}, prelude::Direction, style::Style, symbols, text::Line, widgets::{Block, Borders, Paragraph, Tabs, Widget}, Frame};
+use ratatui::prelude::*;
+use ratatui::{layout::{self, Constraint, Layout, Rect}, prelude::Direction, style::{Style,Color}, symbols, text::{Line, Text}, widgets::{Block, Borders, Paragraph, Tabs, }, Frame};
+use tui_widget_list::{ListBuilder, ListView,ListState};
 use crate::{app::CurrentTab, App};
 use tui_textarea::{TextArea,Input,Key};
 
@@ -8,6 +10,7 @@ pub fn ui(frame: &mut Frame, app: &mut App){
     let tabs = Tabs::new(vec!["Entries", "Search", "Edit", "Settings"])
         .select(app.tab_index as usize)
         .block(top_block);
+
     let border_set = symbols::border::Set {
         top_left: symbols::line::NORMAL.vertical_right,
         top_right: symbols::line::NORMAL.vertical_left,
@@ -17,13 +20,14 @@ pub fn ui(frame: &mut Frame, app: &mut App){
         .border_set(border_set)
         .borders(Borders::ALL);
     let chunks = Layout::default()
-    .direction(Direction::Vertical)
-    .constraints([
+        .direction(Direction::Vertical)
+        .constraints([
         Constraint::Min(2),
         Constraint::Percentage(100)
     ])
-    .split(frame.area()); 
+    .split(frame.area());
     let bottom_block_area = bottom_block.inner(chunks[1]);
+
     frame.render_widget(tabs, chunks[0]);        
     frame.render_widget(bottom_block, chunks[1]);
     match app.current_tab{
@@ -66,13 +70,48 @@ fn render_search_screen(frame: &mut Frame, app: &mut App, area: Rect) {
     frame.render_widget(&app.query, search_layout[1]);
 }
 
+pub struct ListItem{
+    text: String,
+    style: Style
+}
+
+impl ListItem {
+    pub fn new<T: Into<String>>(text: T) ->Self{
+        Self { text: text.into(), style: Style::default() }
+    }
+}
+
+impl Widget for ListItem {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        Line::from(self.text).style(self.style).render(area, buf);
+    }
+}
+
 fn render_entries_screen(frame: &mut Frame, app: &mut App, area: Rect) {
-    let text = Text::from(
-    match &app.dictionary {
-        None => {vec![Line::from("foo"), Line::from("bar")]}
-        Some(dic) => {
-            dic.entries.iter().map(|entry| Line::from(format!("{entry:?}"))).collect()
+    let layout = Layout::default()
+    .direction(Direction::Horizontal)
+    .constraints([
+        Constraint::Max(6),
+        Constraint::Percentage(0)
+    ])
+    .split(area);
+    let id_list_builder = ListBuilder::new(|context| {
+        let mut item = ListItem::new(format!("{:0}", context.index));
+        if context.index % 2 == 0 {
+            item.style = Style::default().bg(Color::Rgb(28, 28, 32));
+        } else {
+            item.style = Style::default().bg(Color::Rgb(0, 0, 0))
         }
+        if context.is_selected{
+            item.style = Style::default()
+            .bg(Color::Rgb(255, 153, 0))
+            .fg(Color::Rgb(28,28,32))
+        }
+        let main_axis_size = 1;
+        (item, main_axis_size)
     });
-    frame.render_widget(Paragraph::new(text), area); 
+    let item_count = 2;
+    let list = ListView::new(id_list_builder, item_count);
+    let mut list_state_test = ListState::default();
+    frame.render_stateful_widget(list, layout[0], &mut list_state_test);
 }
